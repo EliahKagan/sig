38e441523d1ce38a3d309c939148e3b9b279e170
Eliah Kagan <degeneracypressure@gmail.com>
2023-12-04 17:40:23 -0500

    Add some more useful messages

--- sig.c
+++ sig.c
@@ -1,3 +1,5 @@
+// Unix signals demonstration on a subprocess with its own subprocess.
+
 #define _POSIX_C_SOURCE 1
 
 #include <signal.h>
@@ -37,7 +39,7 @@ noreturn static void status_loop(const char *name)
 
     for (; ; ) {
         printf("%s process (PID %d) is running.\n", name, (int)pid);
-        
+
         // A terminal would usually be line buffered, which is ideal because it
         // makes it likely a single write operation will be performed. Setting
         // line buffering with setvbuf may not always work, because it may only
@@ -54,6 +56,7 @@ int main(int argc, char **argv)
     printf("Testing with signal %d.\n", sig);
     fflush(stdout); // In case it's not a terminal. See status_loop.
 
+    pid_t parent = getpid();
     pid_t child = fork();
     if (child < 0) die("Can't fork child");
 
@@ -61,9 +64,11 @@ int main(int argc, char **argv)
         // This is the parent.
         printf("Parent (%d) sleeping %d s before sending signal %d"
                " to child (%d).\n",
-               getpid(), k_interval_before_signal, sig, child);
-
+               (int)parent, k_interval_before_signal, sig, (int)child);
         sleep(k_interval_before_signal);
+
+        printf("Parent (%d) sending signal %d to child (%d).\n",
+               (int)parent, sig, (int)child);
         if (kill(child, sig) != 0) die("Can't send signal to child");
 
         status_loop("Parent");
